3a4a74675844e9d5081e39c88400a768df1dd72a
Pany Matondang <ifs21042@students.del.ac.id>
2024-03-22 10:55:32 +0700

    Remove .env

--- BaksoMie/settings.py
+++ BaksoMie/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%hkskhyrd2v#y_jm2q6u&+bys!c)jtvma(2pb98_dmy&*f&$0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
