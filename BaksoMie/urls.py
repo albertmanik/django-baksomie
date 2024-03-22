9a30c86c0639d0a91bb389ae8025416298df7c0e
Pany Matondang <ifs21042@students.del.ac.id>
2024-03-22 11:23:31 +0700

    update structure

--- BaksoMie/urls.py
+++ BaksoMie/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [ 
-    path('', include('login.urls')), 
+    path('/', include('web.urls')), 
+    path('login/', include('login.urls')), 
 ]
